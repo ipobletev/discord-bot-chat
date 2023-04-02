e9e91b2b404c7b0aaea6203b505273ef5511c56f
Ismael Poblete <ismael.pbt@gmail.com>
2023-04-01 23:43:47 -0300

    update help

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ async def help(ctx):
         "chatisma voice_language \"en\"":"Set system voice language for text to speech.",
         "chatisma cht \"text\"": "Send prompt to OpenAI, respond with text and voice (Automatically calls bot to voice channel if not present)"
     }
-    for [fieldName, fieldVal] in helpData.items():
-        embed.add_field(name=fieldName, value=fieldVal, inline=False)
+    for [fieldCmd, fieldDescription] in helpData.items():
+        embed.add_field(name=fieldDescription, value=fieldCmd, inline=False)
     await ctx.send(embed=embed)
     
 @bot.command(name="profile")
